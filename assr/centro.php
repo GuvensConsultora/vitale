597f25d1ad94a1fc521b66c9664695d62980a477
horacio <guvensconsultora@gmail.com>
2023-08-12 22:44:08 -0300

    Borrado de assr

--- assr/centro.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "<html><body>";
-echo "<br><br><br><br>";
-echo "<div align=center><H1>AREA SANITARIA SAN RAFAEL</H1></dib>";
-echo "</body></html>";
-?>
--- assr/formclaves.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<div align=center><h2>AREA SANITARIA SAN RAFAEL</H2></div>";
-echo "<div align=center><h3>Formulario: control de acceso<h3></div>";
-?>
